ab1a578729cc12788452b1c45bc0b1a3b4962ade
Aditya Pachupate <adityapachupate@gmail.com>
2025-01-26 23:47:02 +0530

    Set base URL to relative path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base:"/dooFlix"
+  base: './' // Set base URL to relative path
 })
